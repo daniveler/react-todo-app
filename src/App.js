4d4f3c3ccad340ecd10738e4c1103ba1536f836b
daniveler <daniel01velerdas@gmail.com>
2023-10-13 14:56:19 +0200

    Finished AppHeader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PageTitle from './components/PageTitle';
+import styles from './styles/modules/app.module.scss';
+import AppHeader from './components/AppHeader';
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <PageTitle>TO-DO LIST</PageTitle>
+      <div className={styles.app__wrapper}>
+        <AppHeader />
+      </div>
     </div>
   );
 }
